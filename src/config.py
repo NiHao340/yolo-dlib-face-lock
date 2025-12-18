5de9f3e829800c694c80c8259a74fc9e5c20030a
Ethan <2035901544@qq.com>
2025-12-18 20:51:14 +0800

    Add configuration settings for face detection

---
+++ src/config.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+FACE_THRESHOLD = 0.45
+
+DISPLAY_INTERVAL = 2
+DETECT_INTERVAL = 5
+
+DLIB_PREDICTOR = "models/shape_predictor_68_face_landmarks.dat"
+DLIB_RECOG_MODEL = "models/dlib_face_recognition_resnet_model_v1.dat"
+
